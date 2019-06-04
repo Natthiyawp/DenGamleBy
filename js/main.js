7cdc99cc68c340d0de6aa997fbb8060fe229fb94
Daniel Pedersen <daniel.horsens@gmail.com>
2019-06-04 09:29:30 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,47 @@
 $(function () {
     // Open Lightbox
-    $('.open-lightbox').on('click', function(e) {
-        e.preventDefault();
-        var img = $(this).find('img').attr('src');
-        var path    =   '<figure class="lightbox-opened"><img src="'+img+'"></figure>';
-        $('body').addClass('no-scroll');
-        $('body').append(path);
-        close('.lightbox-opened');
-        
-
-    });
-    function close(elm){
-           // Close Lightbox
-    $(elm).on('click', function() {
-        console.log(this);
-        $('body').removeClass('no-scroll');
-        $(elm).remove();
-    });
-    }
- 
+    var lightboxOpened  =   'lightbox-opened';
+    var lightbox        =   'lightbox';   
+    var figure      =   $('figure');
+    // Finder alle figures
+    function openLightbox(figure) {
+        console.log(figure);
+        figure.on('click', function(e) {
+            e.preventDefault();
+            var img = $(this).find('img').attr('src');
+            var div = '<div class="'+lightbox+'"></div>'
+            var path    =   '<figure class="'+lightboxOpened+'"><img src="'+img+'"></figure>';
+            $('body').addClass('no-scroll');
+            $('body').append(path);
+            $('body').append(div);
+            close(lightboxOpened, lightbox);
+            //  console.log(lightboxOpened, lightbox)
+        });
+    };
+    openLightbox(figure);
+    // Close lightbox
+    function close(lightboxOpened, lightbox){    
+        // Click on figure -> Remove lightbox
+        $('.'+lightboxOpened).on('click', function() {
+            // openL.off();
+            // Aktiver scroll igen
+            $('body').removeClass('no-scroll');
+            // Fjern figure
+            $('.'+lightboxOpened).remove();
+            // Fjern div
+            $('.'+lightbox).remove();
+            // var t   =  setTimeout(function(){
+            //     console.log(this);
+            // },500);
+        });
+        // Click on div -> Remove lightbox
+        $('.'+lightbox).on('click', function() {
+            // Aktiver scroll igen
+            $('body').removeClass('no-scroll');
+            // Fjern Figure
+            $('.'+lightboxOpened).remove();
+            // Fjern Div
+            $('.'+lightbox).remove();
+        });
+    } 
 });
