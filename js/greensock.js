09601797ca31c907fcfbe21ac211dafa80cf377c
Daniel Pedersen <daniel.horsens@gmail.com>
2019-06-07 09:42:01 +0200

    Update greensock.js

--- js/greensock.js
+++ js/greensock.js
@@ -1,1 +1,6 @@
-TweenMax.To("#start", 5 {morphSVG:"#end", ease:Back.easeOut});
+$(function () {
+    const   start   =   $('#start');
+    const   end     =   $('#end');
+    console.log(start[0],end[0]);
+    TweenMax.to("#start", 5, {morphSVG:"#end", ease:Back.easeOut});
+});
