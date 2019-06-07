6ceadef86238ee74de535cc020bcdfc7b67af52f
Daniel Pedersen <daniel.horsens@gmail.com>
2019-06-07 12:13:20 +0200

    Update tema.js

--- js/tema.js
+++ js/tema.js
@@ -1,93 +1,103 @@
-$(function(){
-    "use strict";
-    //more cards
-    var cardList    =   newCard(3);
-    
-    var allCard     =   shuffleArray(cardList);
-    console.log(allCard)
+$(function () {
+    "use script";
+    // Antal kort
+    var cardList        =   newCard(12);
+    // Bland kortene
+    var allCard = shuffleArray(cardList);
+    // Append card(li) til ul
     $('.scene').append(allCard);
-    
-    var card1       =   undefined,
-        card2       =   undefined,
-        flip        =   "is-flipped",
-        allCard     =   $("ul.scene li"),
-        numberOfCard =   allCard.length,
-        cardTurned  =   0;
-    
-    allCard.on('click',function(e){
-        console.log(this)
+    var card1           =   undefined,
+        card2           =   undefined,
+        flip            =   "is-flipped",
+        allCard         =   $("ul.scene li"),
+        numberOfCard    =   allCard.length,
+        cardTurned      =   0;
+
+    allCard.on('click', function(e) {
         var thisBrik    =   $(this);
-        // init opstart den føste brik bilver defineret
-        if(card1 === undefined && card2 === undefined){
-            card1 = thisBrik;
+        console.log(card1);
+        console.log(card2);
+        //Vælge brik 1, hvis både kort 1 og kort 2 er undefined
+        if(card1 === undefined && card2 === undefined) {
+            card1   =   thisBrik;
             card1.addClass(flip);
         }
-        //Hvis card2 er undefined og
-        //Hivs denne brik ikke er den samme som den forgåene
-        else if(card2   === undefined && !thisBrik.is(card1)){
-            card2 = thisBrik;
+        // Hvis card2 er undefinded og
+        // hvis denne brik ikke er den samme som den forgående
+        else if(card2 === undefined && !thisBrik.is(card1)) {
+            card2   =   thisBrik;
             card2.addClass(flip);
-            var isSameValue =   identicalCard(card1,card2);
+            //Tjek efter om kort 1 og 2 er ens
+            var isSameValue =   identicalCard(card1, card2);
             if(isSameValue) {
+                //Ændre værdien på antal vendte kort med +2
                 cardTurned  =   cardTurned+2;
-                if(cardTurned   === numberOfCard){
-                    tjecVictory();
+                // Tjek efter om alle kort er vendt
+                if(cardTurned == numberOfCard) {
+                    tjekVictory();
                 }
-                card1 = undefined;
-                card2 = undefined;
-                }
-            else{
+                //Reset kort 1 og kort 2
+                card1   =   undefined;
+                card2   =   undefined;
+            } else {
+                //Resetter kort 1 og kort 2 og fjerner klassen is-flipped
                 resetCard(card1, card2);
-                card1 = undefined
-                card2 = undefined
-                //consle.log("hej")
+                card1   =   undefined;
+                card2   =   undefined;
             }
-            }
-        })
-    function resetCard(card1, card2){
-        var t= setTimeout(function(){
+        }
+    })
+    // Fjern klassen is-flipped fra kort 1 og kort 2
+    function resetCard(card1, card2) {
+        var t   =   setTimeout(function () {
             card1.removeClass(flip);
             card2.removeClass(flip);
-        },1000)
+        // Sæt tiden
+        }, 1000)
     }
-             
-    function identicalCard(card1,card2){
-        var a = card1.data('pair');
-        var b = card2.data('pair');
+    //Læs værdien fra data inde i HTML'en
+    function identicalCard(card1, card2) {
+        var a   =   card1.data('b');
+        var b   =   card2.data('b');
         console.log(a,b);
-        if(a===b){
+        if(a===b) {
+            //Når par er fundet gør så man ikke kan
+            //klikke på den igen
+            card1.off();
+            card2.off();
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
-    function tjecVictory(){
-        $('<h2 class="VICTORY">VICTORY</h2>').appendTo('body');
+    //Skriv Victory inde i HTML'en
+    function tjekVictory() {
+        $('<h2 class="victory">VICTORY</h2>').appendTo('body');
     }
-    function newCard(number){
-        //only even numbers
-        if(number%2===0){
+    // Lav et nyt "kort"
+    function newCard(number) {
+        //Check for om tallet er et lige eller ulige tal
+        if(number%2==0) {
             var cardList    =   [];
             var cardValue   =   0;
-            for(var i=1;i<=number;i++){
+            for(var i=1;i<=number;i++) {
                 var modola  =   i%2;
-                if(modola   ==  1){cardValue++}
-                //cardFace front
-                var card = $('<li class="card" data-pair='+cardValue+'><div class="cardFace front">Front</div><div class="cardFace back"> <h2>'+cardValue+'<h2></div></li>');
+                if(modola == 1) {cardValue++}
+                // Variable - Classen der skal indsættes i HTML
+                var card = $('<li class="card" data-pair='+cardValue+'><div class="cardFace front">Front</div><div class="cardFace back"><h2>'+cardValue+'</h2></div></li>');
+                // Indsæt et kort ind i HTML
                 cardList.push(card)
             }
             return cardList;
         }
-             
         else{return false}
     }
-    function shuffleArray(array)    {
+    function shuffleArray(array) {
         for (var i = array.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp =array[i];
-            array[i]    = array[j];
-            array[j]    = temp;
+            var j = Math.floor(Math.random()    *   (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
         }
         return array;
     }
