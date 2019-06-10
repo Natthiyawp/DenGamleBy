27d2017808dc9db7cc74b38f9398a15dc94a333a
Nat <nat_poser@msn.com>
2019-06-11 00:50:49 +0200

    Update artikel.js

--- js/artikel.js
+++ js/artikel.js
@@ -1,8 +1,8 @@
-$(function() {
-    var upen = $(".article-nav ul li div:first-of-type");
-    upen.on('click',function(){
-        var knap = $(this);
-        var text = knap.next();
-        text.slideToggle("slow");
-    })
-})
+$ (function(){
+var upen = $("div#Artikel nav ul li div:first-of-type");
+upen.on('click',function(){
+    var knap = $(this);
+    var text = knap.next();
+    text.slideToggle("slow");
+});    
+});
