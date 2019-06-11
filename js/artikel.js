636c87f734febd24a91e60ebb054340909eeb7a2
Daniel Pedersen <daniel.horsens@gmail.com>
2019-06-11 08:31:44 +0200

    Update artikel.js

--- js/artikel.js
+++ js/artikel.js
@@ -1,8 +1,8 @@
-$ (function(){
-var upen = $("div#Artikel nav ul li div:first-of-type");
-upen.on('click',function(){
-    var knap = $(this);
-    var text = knap.next();
-    text.slideToggle("slow");
-});    
+$(function(){
+    var upen = $("div#Artikel nav ul li div:first-of-type");
+    upen.on('click',function(){
+        var knap = $(this);
+        var text = knap.next();
+        text.slideToggle("slow");
+    });
 });
